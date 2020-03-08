edca3891c68dbb102f9fd352a0bc2cfb0c66e2f8
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2020-03-08 23:38:54 +0100

    remove harmony debug output

--- Source/ModSwitch/[Patches]/Patches.cs
+++ Source/ModSwitch/[Patches]/Patches.cs
@@ -77,7 +77,6 @@ namespace DoctorVanGogh.ModSwitch {
 
         public class Page_ModsConfig_DoModRow {
             [HarmonyPatch(typeof(Page_ModsConfig), "DoModRow", new[] {typeof(Listing_Standard), typeof(ModMetaData), typeof(int), typeof(int)})]
-            [HarmonyDebug]
             public class SupressNonMatchingFilteredRows {
                 public static bool Prefix(ModMetaData mod) {
                     return ModsConfigUI.Search.MatchCriteria(mod?.Name) || true == mod?.SupportedVersionsReadOnly.Any(s => ModsConfigUI.Search.MatchCriteria(s.ToString()));
